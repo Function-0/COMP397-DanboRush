22ada8ea5154b105a113ffab7d4c55a8f604c244
Nina <ninawu424@gmail.com>
2021-02-15 00:30:28 -0500

    Added soundtrack and PauseMenu to GamePlay Screen

--- Assets/_Scripts/PauseMenu.cs
+++ Assets/_Scripts/PauseMenu.cs
@@ -2,7 +2,7 @@
  * @Author: Tzu-Ting Wu 
  * @Date: 2021-02-07 16:48:00 
  * @Last Modified by: Tzu-Ting Wu
- * @Last Modified time: 2021-02-07 17:26:44
+ * @Last Modified time: 2021-02-15 00:21:38
  */
 using System.Collections;
 using System.Collections.Generic;
@@ -13,12 +13,18 @@ public class PauseMenu : MonoBehaviour
 {
     public AudioSource clickSound;
     public static bool isPaused = false;
+    public GameObject pauseCanvas;
     public GameObject pauseMenuUI;
+
+    void Start() {
+        pauseCanvas.GetComponent<Canvas>().enabled = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) {
+            pauseCanvas.GetComponent<Canvas>().enabled = true;
             if (isPaused) {
                 Resume();
             }
