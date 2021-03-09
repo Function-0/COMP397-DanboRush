f53fde8b2c38271938c8c3261aa8696507c971cd
Sarjil001 <sarjil.raval001@gmail.com>
2021-03-08 21:17:46 -0500

    Added wrecking tractor rotation and sound effects

---
+++ Assets/_Scripts/WreckingDamage.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WreckingDamage : MonoBehaviour
+{
+
+	public float lookRadius = 5f;
+
+	public AudioClip audioClip;
+	public float volume;
+	AudioSource wreckingSound;
+	public bool alreadyPlayed = false;
+
+	private float damage = 5f;
+
+	public float fireRate = 1f;
+	private float nextTimeToFire = 0f;
+
+	private GameObject player;
+	Transform target;
+
+	private PlayerBehaviour playerBehaviour;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		player = GameObject.Find("Amazon danbo");
+		target = player.transform;
+		playerBehaviour = FindObjectOfType<PlayerBehaviour>();
+		wreckingSound = GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+		// Get the distance to the player
+		float distance = Vector3.Distance(target.position, transform.position);
+
+		// If inside the radius
+
+		if (distance <= lookRadius && Time.time >= nextTimeToFire)
+		{
+			nextTimeToFire = Time.time + 1f / fireRate;
+			playerBehaviour.TakeDamage(damage);
+		}
+
+	}
+
+	void OnTriggerEnter()
+	{
+		if (!alreadyPlayed)
+		{
+			wreckingSound.PlayOneShot(audioClip, volume);
+		//	alreadyPlayed = true;
+		}
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, lookRadius);
+	}
+}
