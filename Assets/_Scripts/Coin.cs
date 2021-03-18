c38928d7aa9ac8aef608ead2a517c6921e60b1d4
Sarjil001 <sarjil.raval001@gmail.com>
2021-03-18 02:02:58 -0400

    Section 2, ground floor enemies added

--- Assets/_Scripts/Coin.cs
+++ Assets/_Scripts/Coin.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private float coins = 10f;
+    private float coins = 2f;
     public CoinScript coinScript;
 
     // Start is called before the first frame update
